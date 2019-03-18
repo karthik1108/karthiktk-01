e15068eb9170c3202500c1a63d08f2fa448d8faa
karthik1108 <karthiktech11@gmail.com>
2019-03-18 20:34:58 +0530

    base  class created

---
+++ src/test/sample.java
@@ -0,0 +1,11 @@
+package test;
+
+public class sample{
+	public static void main(String[] args) {
+		String s="welcome";
+		int j= s.indexOf("1");
+		System.out.println(j);
+		int k=s.indexOf("4");
+		System.out.println(k);
+	}
+}
